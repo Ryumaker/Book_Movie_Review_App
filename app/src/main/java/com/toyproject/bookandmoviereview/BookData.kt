d26ed1314c3fed2779cb6bd57d78d9651ddd1d96
lspc678 <dltmdqls1945@naver.com>
2023-07-22 03:02:58 +0900

    BookData class file

---
+++ app/src/main/java/com/toyproject/bookandmoviereview/BookData.kt
@@ -0,0 +1,4 @@
+package com.toyproject.bookandmoviereview
+
+// data class BookData(var title: String, var author: String, var rating: Float, var numberOfComments: Int, var image: Int)
+data class BookData(var title: String, var author: String, var rating: Float, var numberOfComments: Int, var imageUrl: String)
