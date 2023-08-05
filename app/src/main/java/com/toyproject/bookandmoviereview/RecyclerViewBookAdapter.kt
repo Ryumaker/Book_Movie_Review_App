05aafa6669daa33263f30ad4e94a8d43749828f7
lspc678 <dltmdqls1945@naver.com>
2023-08-05 21:19:03 +0900

    changed image load method (executor --> glide)

--- app/src/main/java/com/toyproject/bookandmoviereview/RecyclerViewBookAdapter.kt
+++ app/src/main/java/com/toyproject/bookandmoviereview/RecyclerViewBookAdapter.kt
@@ -2,10 +2,8 @@ package com.toyproject.bookandmoviereview
 
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,13 +13,9 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import java.util.concurrent.Executors
+import com.bumptech.glide.Glide
 
 class RecyclerViewBookAdapter constructor(private val getFragment: HomeBookFragment, private val bookList: List<BookData>) : RecyclerView.Adapter<RecyclerViewBookAdapter.ViewHolder>() {
-    private val executor = Executors.newSingleThreadExecutor()
-    private val handler = Handler(Looper.getMainLooper())
-    private var image: Bitmap? = null
-
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textListTitle: TextView = itemView.findViewById(R.id.textListTitle)
         val textListAuthor: TextView = itemView.findViewById(R.id.textListAuthor)
@@ -47,20 +41,8 @@ class RecyclerViewBookAdapter constructor(private val getFragment: HomeBookFragm
         holder.ratingBar.rating = bookList[position].rating
         holder.textNumberOfComments.text = "(" + bookList[position].numberOfComments.toString() + ")"
 
-        executor.execute {
-            val imageUrl = bookList[position].imageUrl
-            try {
-                val `in` = java.net.URL(imageUrl).openStream()
-                image = BitmapFactory.decodeStream(`in`)
-                handler.post {
-                    holder.imageBook.setImageBitmap(image)
-                }
-            } catch (e: java.lang.Exception) {
-                e.printStackTrace()
-            }
-        }
-
-        // holder.imageBook.setImageResource(bookList[position].image)
+        // Image load by imageUrl
+        Glide.with(holder.itemView.context).load(bookList[position].imageUrl).into(holder.imageBook)
 
         holder.layoutListItem.setOnClickListener {
             Toast.makeText(getFragment.requireContext(), bookList[position].title, Toast.LENGTH_SHORT).show()
