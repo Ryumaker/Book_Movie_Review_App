d99fd6507178791699bb89b33e60a83af06fdcd1
lspc678 <dltmdqls1945@naver.com>
2023-08-14 21:20:19 +0900

    new recycler adapter for book review list

---
+++ app/src/main/java/com/toyproject/bookandmoviereview/RecyclerViewBookReviewAdapter.kt
@@ -0,0 +1,43 @@
+package com.toyproject.bookandmoviereview
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RatingBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewBookReviewAdapter constructor(private val context: Context, private val bookReviewList: List<BookReviewData>) : RecyclerView.Adapter<RecyclerViewBookReviewAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textNickname: TextView = itemView.findViewById(R.id.textNickname)
+        val ratingBar: RatingBar = itemView.findViewById(R.id.ratingBar)
+        val textUploadDate: TextView = itemView.findViewById(R.id.textUploadDate)
+        val textReview: TextView = itemView.findViewById(R.id.textReview)
+        val textNumberOfThumbUp: TextView = itemView.findViewById(R.id.textNumberOfThumbUp)
+        val layoutReviewListGridItem: CardView = itemView.findViewById(R.id.layoutReviewListGridItem)
+    }
+
+    override fun getItemCount(): Int {
+        return bookReviewList.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.review_list_grid_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textNickname.text = bookReviewList[position].nickname
+        holder.textUploadDate.text = bookReviewList[position].uploadDate
+        holder.textReview.text = bookReviewList[position].review
+        holder.textNumberOfThumbUp.text = bookReviewList[position].numberOfThumbUp.toString()
+        holder.ratingBar.rating = bookReviewList[position].rating
+
+        holder.layoutReviewListGridItem.setOnClickListener {
+            Toast.makeText(context, bookReviewList[position].nickname, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
