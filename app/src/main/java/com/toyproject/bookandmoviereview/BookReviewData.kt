d46559f0127f5fc93cbde5583cd23d60127b5eb3
lspc678 <dltmdqls1945@naver.com>
2023-08-15 16:45:43 +0900

    type of uploadDate is changed to LocalDate

--- app/src/main/java/com/toyproject/bookandmoviereview/BookReviewData.kt
+++ app/src/main/java/com/toyproject/bookandmoviereview/BookReviewData.kt
@@ -1,3 +1,5 @@
 package com.toyproject.bookandmoviereview
 
-data class BookReviewData(var nickname: String, var rating: Float, var uploadDate: String, var review: String, var numberOfThumbUp: Int)
+import java.time.LocalDate
+
+data class BookReviewData(var nickname: String, var rating: Float, var uploadDate: LocalDate, var review: String, var numberOfThumbUp: Int)
