77cba77f8d26a721a9ab9ab79282688f1b527fd1
lspc678 <dltmdqls1945@naver.com>
2023-08-20 12:09:02 +0900

    set review data for BookReviewActivity

--- app/src/main/java/com/toyproject/bookandmoviereview/BookReviewActivity.kt
+++ app/src/main/java/com/toyproject/bookandmoviereview/BookReviewActivity.kt
@@ -15,11 +15,22 @@ class BookReviewActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val title = intent.getStringExtra("title")
+        val nickname = intent.getStringExtra("nickname")
+        val uploadDate = intent.getStringExtra("uploadDate")
+        val review = intent.getStringExtra("review")
+        val numberOfThumbUp = intent.getStringExtra("numberOfThumbUp")
+        val score = intent.getFloatExtra("score", 0.0F)
 
         val toolbar = binding.toolbar
         setSupportActionBar(toolbar)
         supportActionBar?.title = title
         supportActionBar?.setDisplayHomeAsUpEnabled(true) // 앱바에 back 버튼 활성화
+
+        binding.reviewListGridItem.textNickname.text = nickname
+        binding.reviewListGridItem.textUploadDate.text = uploadDate
+        binding.reviewListGridItem.textReview.text = review
+        binding.reviewListGridItem.textNumberOfThumbUp.text = numberOfThumbUp
+        binding.reviewListGridItem.ratingBar.rating = score
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
