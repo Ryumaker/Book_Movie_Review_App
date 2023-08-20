9f37b14f69652d236e302b29123b02fde5b4d99c
lspc678 <dltmdqls1945@naver.com>
2023-08-20 11:49:22 +0900

    BookData params naming changed

--- app/src/main/java/com/toyproject/bookandmoviereview/BookData.kt
+++ app/src/main/java/com/toyproject/bookandmoviereview/BookData.kt
@@ -1,4 +1,3 @@
 package com.toyproject.bookandmoviereview
 
-// data class BookData(var title: String, var author: String, var rating: Float, var numberOfComments: Int, var image: Int)
-data class BookData(var title: String, var author: String, var rating: Float, var numberOfReviews: Int, var numberOfComments: Int, var imageUrl: String)
+data class BookData(var title: String, var author: String, var score: Float, var numberOfRatings: Int, var numberOfReviews: Int, var imageUrl: String)
