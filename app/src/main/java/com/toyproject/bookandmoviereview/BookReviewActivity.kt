fbdf70b0c446aef03cd269bc666b4e4597dd3654
lspc678 <dltmdqls1945@naver.com>
2023-08-20 16:34:41 +0900

    implement reply list at BookReviewActivity

--- app/src/main/java/com/toyproject/bookandmoviereview/BookReviewActivity.kt
+++ app/src/main/java/com/toyproject/bookandmoviereview/BookReviewActivity.kt
@@ -1,14 +1,62 @@
 package com.toyproject.bookandmoviereview
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.toyproject.bookandmoviereview.databinding.ActivityBookReviewBinding
+import com.toyproject.bookandmoviereview.models.ReplyData
+import java.time.LocalDate
+import java.util.PriorityQueue
+
+private data class ComparableUploadDateForReplyData(var replyData: ReplyData) : Comparable<ComparableUploadDateForReplyData> {
+    override fun compareTo(other: ComparableUploadDateForReplyData): Int {
+        return if (other.replyData.uploadDate > replyData.uploadDate) {
+            1
+        } else if (other.replyData.uploadDate < replyData.uploadDate) {
+            -1
+        } else {
+            0
+        }
+    }
+}
 
 class BookReviewActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBookReviewBinding
+    private var recyclerView: RecyclerView? = null
+    private var recyclerViewReplyAdapter: RecyclerViewReplyAdapter? = null
+    private var replyList = mutableListOf<ReplyData>()
 
+    @SuppressLint("NotifyDataSetChanged")
+    private fun prepareReplyListData() {
+        val pq = PriorityQueue<ComparableUploadDateForReplyData>()
+
+        var comment = ReplyData("Helene Moore", LocalDate.of(2019, 12, 1), "Sed in lorem ac lacus commodo commodo.")
+        pq.add(ComparableUploadDateForReplyData(comment))
+
+        comment = ReplyData("Mauris urna ligula", LocalDate.of(2019, 11, 15), "Nullam id ante bibendum, convallis tortor sit amet, vestibulum augue.")
+        pq.add(ComparableUploadDateForReplyData(comment))
+
+        comment = ReplyData("tempus et", LocalDate.of(2019, 10, 22), "Donec eget ante sed metus tincidunt eleifend.")
+        pq.add(ComparableUploadDateForReplyData(comment))
+
+        comment = ReplyData("Nullam nec", LocalDate.of(2019, 10, 23), "Pellentesque accumsan felis sit amet mattis ullamcorper. Suspendisse semper urna sit amet nibh tempor, et congue lacus vestibulum.")
+        pq.add(ComparableUploadDateForReplyData(comment))
+
+        comment = ReplyData("Sem tristique metus", LocalDate.of(2019, 9, 5), "Morbi ut bibendum nisi. Maecenas ac dui id nibh malesuada lobortis a vel elit. Praesent consequat dolor nisl, a sagittis risus feugiat ac. Sed facilisis sollicitudin dictum. Fusce eu tortor sit amet mauris bibendum varius. Pellentesque tempor elementum ultrices. Aenean semper tortor id sapien commodo faucibus. Quisque malesuada massa vel tempus sodales.")
+        pq.add(ComparableUploadDateForReplyData(comment))
+
+        while (pq.isNotEmpty()) {
+            pq.poll()?.let { replyList.add(it.replyData) }
+        }
+
+        recyclerViewReplyAdapter!!.notifyDataSetChanged()
+    }
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBookReviewBinding.inflate(layoutInflater)
@@ -31,6 +79,15 @@ class BookReviewActivity : AppCompatActivity() {
         binding.reviewListGridItem.textReview.text = review
         binding.reviewListGridItem.textNumberOfThumbUp.text = numberOfThumbUp
         binding.reviewListGridItem.ratingBar.rating = score
+
+        recyclerView = binding.recyclerViewForReply
+        recyclerViewReplyAdapter = RecyclerViewReplyAdapter(this, replyList)
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+        recyclerView!!.layoutManager = layoutManager
+        recyclerView!!.adapter = recyclerViewReplyAdapter
+        prepareReplyListData()
+
+        binding.textNumberOfReplies.text = "${replyList.size} Replies"
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
