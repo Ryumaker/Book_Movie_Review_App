295b0d5b4b16958bc8bea4298b80021baed47a4e
lspc678 <dltmdqls1945@naver.com>
2023-08-25 21:08:45 +0900

    add tabLayout and frameLayout at DiscussionBoardFragment

--- app/src/main/java/com/toyproject/bookandmoviereview/DiscussionBoardFragment.kt
+++ app/src/main/java/com/toyproject/bookandmoviereview/DiscussionBoardFragment.kt
@@ -5,12 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.tabs.TabLayout
+import com.toyproject.bookandmoviereview.databinding.FragmentDiscussionBoardBinding
 
 class DiscussionBoardFragment : Fragment() {
+    private var _binding: FragmentDiscussionBoardBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var tabLayout: TabLayout
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_discussion_board, container, false)
+    ): View {
+        _binding = FragmentDiscussionBoardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tabLayout = binding.tabLayout
+
+        tabLayout.addTab(tabLayout.newTab().setText("책"))
+        tabLayout.addTab(tabLayout.newTab().setText("영화"))
+
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab!!.position) {
+                    0 -> replaceFragment(DiscussionBoardBookFragment())
+                    1 -> replaceFragment(DiscussionBoardMovieFragment())
+                }
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+
+        // 기본 화면을 DiscussionBoardBookFragment()로 설정
+        if (savedInstanceState == null) {
+            replaceFragment(DiscussionBoardBookFragment())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction: FragmentTransaction = childFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout, fragment)
+        transaction.commit()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
